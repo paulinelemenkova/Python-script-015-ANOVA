69fff4b0c877ddb4fb428f08ae20424045ed82a5
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2019-06-12 21:17:10 +0800

    Add files via upload

--- Script-015-ANOVA-Boxplots.py
+++ Script-015-ANOVA-Boxplots.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[47]:
-
-
-# Libraries
+#
 from __future__ import print_function
 import scipy
 import os
-
 from statsmodels.compat import urlopen
 import numpy as np
 import statsmodels.api as sm
@@ -19,37 +14,22 @@ from matplotlib.patches import Polygon
 from statsmodels.formula.api import ols
 from statsmodels.graphics.api import interaction_plot, abline_plot
 from statsmodels.stats.anova import anova_lm
-
+#
 os.chdir('/Users/pauline/Documents/Python')
-
-# Loading data
+#
+# Load data
 df = pd.read_csv("Tab-Morph.csv")
-#print(df.head)
-
-
+#
 fig, ax = plt.subplots(figsize=(8,6))
 fig = df.boxplot('slope_angle', 'slope_class', ax=ax, grid=False)
-
+#
 fig, ax = plt.subplots(figsize=(8,6))
 fig = df.boxplot('sedim_thick', 'slope_class', ax=ax, grid=False)
-
+#
 fig, ax = plt.subplots(figsize=(8,6))
 fig = df.boxplot('igneous_volc', 'slope_class', ax=ax, grid=False)
-
+#
 fig, ax = plt.subplots(figsize=(8,6))
 fig = df.boxplot('tg_angle', 'slope_class', ax=ax, grid=False)
-
+#
 plt.show()
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
